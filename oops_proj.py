f88312dbc47c171c78c59c4cda757b54e1f49d11
Bhabani <sipubhabani63@gmail.com>
2026-06-08 11:15:58 +0530

    mypost feature added in my project

--- oops_proj.py
+++ oops_proj.py
@@ -18,14 +18,14 @@ class chatbook:
         elif user_input == '2':
             self.signin()
         elif user_input == '3':
-            pass
+            self.my_posts()
         elif user_input == '4':
             pass
         else:
             exit()
     def signup(self):
-        email = input("enter your email here")
-        password = input("setup your password here")
+        email = input("enter your email here : ")
+        password = input("setup your password here : ")
         self.username = email
         self.password = password
         print("you have successfully signed up !!")
@@ -33,15 +33,23 @@ class chatbook:
         self.menu()
     def signin(self):
         if self.username == '' or self.password == '':
-            print("you have not signed up yet, please signup first to press the 1 key")
+            print("you have not signed up yet, please signup first to press the 1 key  ")
         else:
-            uname = input("enter your email here")
-            pwd = input("enter your password here")
+            uname = input("enter your email here : ")
+            pwd = input("enter your password here : ")
             if self.username != uname or self.password != pwd:
                 print("invalid credentials, please try again !!")
             else:
                 print("you have successfully signed in !!")
                 self.loggedin = True
+        print("\n")
+        self.menu()
+    def my_posts(self):
+        if self.loggedin == True:
+            txt = input("enter you message here : ")
+            print(f"following content has been posted {txt}")
+        else:
+            print("you need to login first to post something !!")
         print("\n")
         self.menu()
 
